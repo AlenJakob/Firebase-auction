a8f60bac58a6c1e68f554367eb82d3f150cc45d7
Alen <33994452+AlenJakob@users.noreply.github.com>
2020-10-11 11:46:42 +0200

    first push

---
+++ scripts/firebase.js
@@ -0,0 +1,14 @@
+
+ let firebaseConfig = {
+    apiKey: "AIzaSyBT8l15j0FQj2cSAxEzn7N9go3oui73wok",
+    authDomain: "jax-auction.firebaseapp.com",
+    databaseURL: "https://jax-auction.firebaseio.com",
+    projectId: "jax-auction",
+    storageBucket: "jax-auction.appspot.com",
+    messagingSenderId: "1086968407801",
+    appId: "1:1086968407801:web:7c796080bf4f2959ec6428",
+    measurementId: "G-DTS9K0XQGR"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
